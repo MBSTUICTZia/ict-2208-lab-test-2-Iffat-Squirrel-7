a58476aa1851d38d932471aecda83fef474abe53
Iffat-Squirrel-7 <it24035@mbstu.ac.bd>
2026-06-15 14:50:23 +0600

    Add files via upload

---
+++ shapes/ShapeMain.java
@@ -0,0 +1,36 @@
+package shapes;
+
+import java.util.Scanner;
+
+public class ShapeMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        if (sc.hasNext()) {
+            String type = sc.next();
+            String color = sc.next();
+            boolean filled = sc.nextBoolean();
+
+            if (type.equalsIgnoreCase("rectangle")) {
+                double width = sc.nextDouble();
+                double length = sc.nextDouble();
+                
+                Rectangle rect = new Rectangle(color, filled, width, length);
+                System.out.println(rect);
+                System.out.println();
+                System.out.println("--- Downcast Check ---");
+                System.out.println("Rectangle width=" + rect.getWidth() + " length=" + rect.getLength());
+                
+            } else if (type.equalsIgnoreCase("circle")) {
+                double radius = sc.nextDouble();
+                
+                Circle circ = new Circle(color, filled, radius);
+                System.out.println(circ);
+                System.out.println();
+                System.out.println("--- Downcast Check ---");
+                System.out.println("Circle radius=" + circ.getRadius());
+            }
+        }
+        sc.close();
+    }
+}
