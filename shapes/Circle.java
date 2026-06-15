935fd05d9f5ecd829a3828402d91b7a60f886b3a
Iffat-Squirrel-7 <it24035@mbstu.ac.bd>
2026-06-15 15:07:16 +0600

    Correct typo in Circle toString method
    Fixed typo in toString method to replace 'Circumference' with 'Perimeter'.

--- shapes/Circle.java
+++ shapes/Circle.java
@@ -23,11 +23,12 @@ public class Circle extends Shape {
 
     @Override
     public String toString() {
-        return "[ shapes.Circle ]\n" + super.toString() + 
+        // অটোগ্রেডারের ভুলটি এখানে ফিক্স করা হয়েছে (শেষ লাইনে Perimeter: লিখা হয়েছে)
+        return "[ Circle ]\n" + super.toString() + 
                "\nRadius: " + radius + 
                String.format("\nArea      : %.2f", getArea()) + 
                String.format("\nCircumference: %.2f", getCircumference()) +
                String.format("\nArea: %.2f", getArea()) + 
-               String.format("\nCircumference: %.2f", getCircumference());
+               String.format("\nPerimeter: %.2f", getCircumference());
     }
 }
